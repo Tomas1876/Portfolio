630a7013db07fe5835b4a32e537b8973eb33b369
Tomas1876 <mnd3259@gmail.com>
2021-08-21 01:38:35 +0900

    프로젝트 요소 추가

--- new/index.js
+++ new/index.js
@@ -16,33 +16,40 @@ window.onload = () =>{
     }
 
     const p_container = document.querySelector(".projectContainer")
-
     const projects = [
         {title:"CYCODING",
          subtitle:"사이좋게 코딩하자",
          intro:"참여하고 싶은 프로젝트 혹은 프로젝트를 함께 할 멤버를 찾아주는 사이트", 
-         img:Cycoding_img,
+         img:"img/p_cycoding.png",
          skill:"Spring", 
-         github:"https://github.com/DongGeun2/Spring_Final_Project"},
+         github:"https://github.com/DongGeun2/Spring_Final_Project",
+         sub:"마우스를 오버하시면 프로젝트 정보가 노출됩니다"},
          {title:"BIT_SECONDHANDS",
          subtitle:"중고거래 사이트",
          intro:"번개장터를 벤치마킹", 
-         img:Cycoding_img,
+         img:"img/p_cycoding.png",
          skill:"", 
-         github:"https://github.com/Tomas1876/BIT_SECONDHANDS"}
+         github:"https://github.com/Tomas1876/BIT_SECONDHANDS",
+         sub:"마우스를 오버하시면 프로젝트 정보가 노출됩니다"}
     ]
 
     for(let i =0; i < projects.length; i++){
-
         const p_box = makeElement("div", "projectBox");
-
-        const title = makeElement("div", "pTitle");
-        titile.createTextNode(projects[i].title)
-        const intro = makeElement("div", "pIntro");
-        intro.createTextNode(projects[i].intro)
-        const img = makeElement("div", "pImg");
+        const pTitle = makeElement("div", "pTitle");
+        console.log(projects[i].title)
+        const title = document.createTextNode(projects[i].title);
+        pTitle.append(title)
+        const pIntro = makeElement("div", "pIntro");
+        const intro = document.createTextNode(projects[i].intro);
+        pIntro.append(intro)
+        const pImg = makeElement("div", "pImg");
+        const img = makeElement("img", "projectImg");
+        img.setAttribute("src",projects[i].img)
+        pImg.append(img);
         const sub = makeElement("div", "sub");
-        p_box.append(title, intro, img, sub)
+        const subText = document.createTextNode(projects[i].sub);
+        sub.append(subText)
+        p_box.append(pTitle, pIntro, pImg, sub)
         p_container.appendChild(p_box)
     }
 }

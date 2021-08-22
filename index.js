9ead7297d90c94875dfc508e96d1480373ccff10
Tomas1876 <mnd3259@gmail.com>
2021-08-22 19:07:51 +0900

    모바일 버전 contact 박스 잘림 수정

--- index.js
+++ index.js
@@ -53,22 +53,19 @@ window.onload = () =>{
          intro:"참여하고 싶은 프로젝트 혹은 프로젝트를 함께 할 멤버를 찾아주는 사이트", 
          img:"img/p_cycoding02.png",
          skill:"Spring", 
-         github:"https://github.com/DongGeun2/Spring_Final_Project",
-         sub:"마우스를 오버하시면 프로젝트 정보가 노출됩니다"},
+         github:"https://github.com/DongGeun2/Spring_Final_Project"},
          {title:"Portfolio Website",
          subtitle:"포트폴리오 웹사이트",
          intro:"지금 보고 계신 이 페이지", 
          img:"img/p_portfolio.png",
          skill:"", 
-         github:"https://github.com/Tomas1876/Portfolio",
-         sub:"마우스를 오버하시면 프로젝트 정보가 노출됩니다"},
+         github:"https://github.com/Tomas1876/Portfolio"},
          {title:"BIT_SECONDHANDS",
          subtitle:"중고거래 사이트",
          intro:"번개장터를 벤치마킹", 
          img:"img/p_secondhands.png",
          skill:"", 
-         github:"https://github.com/Tomas1876/BIT_SECONDHANDS",
-         sub:"마우스를 오버하시면 프로젝트 정보가 노출됩니다"}
+         github:"https://github.com/Tomas1876/BIT_SECONDHANDS"}
     ]
 
     //프로젝트 박스
@@ -85,10 +82,7 @@ window.onload = () =>{
         const img = makeElement("img", "projectImg");
         img.setAttribute("src",projects[i].img)
         pImg.append(img);
-        const sub = makeElement("div", "sub");
-        const subText = document.createTextNode(projects[i].sub);
-        sub.append(subText)
-        p_box.append(pTitle, pIntro, pImg, sub)
+        p_box.append(pTitle, pIntro, pImg)
         p_container.appendChild(p_box)
     }
 
@@ -98,6 +92,7 @@ window.onload = () =>{
                 `${imgsrc}js.png`,
                 `${imgsrc}css.png`,
                 `${imgsrc}sass.png`,
+                `${imgsrc}react.png`,
                 `${imgsrc}java.png`,
                 `${imgsrc}spring.png`]
     const skillWrap = document.querySelector(".skillWrap");
